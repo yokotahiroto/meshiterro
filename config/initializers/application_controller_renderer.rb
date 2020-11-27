15ad306c43abdde3a5c6ee9e028a8d6267974039
yokotahiroto <hy_dkbs66mh@icloud.com>
2020-11-27 10:12:18 +0000

    [start]4-4

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'af5b1bd5950569e2ae24a01f3a23257a506d707687aec8f362fba2272557aefff5e05b2e7a5b1f23204e3c4a8e1a13419ea9febae3c8b621303ebc482e42dbc7'
